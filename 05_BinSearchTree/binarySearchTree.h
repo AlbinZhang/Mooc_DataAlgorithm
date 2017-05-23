b4b0404b05930aa1046f98f4fd0a42f56d051568
Albin <312024054@qq.com>
2017-05-23 09:34:44 +0800

    fix syntax error

--- 05_BinSearchTree/binarySearchTree.h
+++ 05_BinSearchTree/binarySearchTree.h
@@ -1,64 +1,74 @@
 #ifndef BINARYSEARCHTREE_H
 #define BINARYSEARCHTREE_H
 
+#include <iostream>
+
+using namespace std;
+
 template <typename Key, typename Value>
-class BTS
+class BST
 {
-public:
-    BST()
-    :root(NULL), count(0){}
-
-    ~BST(){
-        //TODO: ~BST()
-    }
-
-    int size() {return count;}
-    bool isEmpty( return count == 0;)
-
-    void insert(Key key, Value value)
+  private:
+    struct Node
     {
-        insert(root, key, value);
-    }
-
-private:
-    void insertWhile(Node* root, Key key, Value value)
-    {
-        if (root == null)
-            root = new Node(key, value);
-    }
-
-    Node *insert(Node *root, Key key, Value value)
-    {
-        if (root == NULL) {
-            count++;
-            return new Node(key, value);
-        }
-            
-        if(root->key == key)
-            root->value = value;
-        else if (root->key > key)
-            root->left = insert(root->left, key, value);
-        else
-            root->right = insert(root->right, key, value);
-
-        return node;
-    }
-
-private:
-    struct Node{
         Key key;
         Value value;
         Node *left;
         Node *right;
         Node(Key key, Value value)
-        :key(key),value(value),left(NULL),right(NULL)
-        {}
+            : key(key), value(value), left(NULL), right(NULL)
+        {
+        }
+    };
+
+  public:
+
+    BST()
+        : m_root(NULL), m_count(0)
+    {
     }
 
-    Node *root;
-    int count;
+    ~BST()
+    {
+        //TODO: ~BST()
+    }
 
+    int size() { return m_count; }
+    bool isEmpty() { return m_count == 0; }
+
+    void insert(Key key, Value value)
+    {
+        insert(m_root, key, value);
+    }
+
+  private:
+    void insertWhile(Node *node, Key key, Value value)
+    {
+        if (node == NULL)
+            node = new Node(key, value);
+    }
+
+    Node *insert(Node *node, Key key, Value value)
+    {
+        if (node == NULL)
+        {
+            m_count++;
+            return new Node(key, value);
+        }
+
+        if (node->key == key)
+            node->value = value;
+        else if (node->key > key)
+            node->left = insert(node->left, key, value);
+        else
+            node->right = insert(node->right, key, value);
+
+        return node;
+    }
+
+  private:
+    Node *m_root;
+    int m_count;
 };
-
 
 #endif //BINARYSEARCHTREE_H
--- 05_BinSearchTree/main.cpp
+++ 05_BinSearchTree/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "binarySearch.h"
 #include "binarySearchTree.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 
-
 int main(int argc, char* argv[]) 
 {	
-	
 
 	return 0;
 }
